152b68d75fe7d9cbf07e421bb93c1d4f6e944b52
Jonathan Novoa <jon142536@gmail.com>
2018-10-05 13:28:33 -0400

    Service test creation

---
+++ src/main/java/com/revature/rideforce/maps/configuration/ServiceTestConfiguration.java
@@ -0,0 +1,14 @@
+package com.revature.rideforce.maps.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+//@EnableJpaRepository()
+@EnableTransactionManagement
+@ComponentScan("com.revature.rideforce.maps")
+public class ServiceTestConfiguration {
+
+	
+}
---
+++ src/test/java/com/revature/rideforce/maps/services/RouteServiceTest.java
@@ -0,0 +1,30 @@
+package com.revature.rideforce.maps.services;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.rideforce.maps.configuration.ServiceTestConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes=ServiceTestConfiguration.class)
+@DataJpaTest
+public class RouteServiceTest {
+	
+	@Autowired
+	private  TestEntityManager testEntityManager; 
+	
+	@Test
+	public void validate() {
+		assertNotNull(testEntityManager);
+	}
+
+}
