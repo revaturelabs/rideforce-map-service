afa761b754551fcbf58fac180e2bbc91d1afe9e2
Jonathan Novoa <jon142536@gmail.com>
2018-10-05 16:16:10 -0400

    working on happy path route test

--- src/test/java/com/revature/rideforce/maps/services/RouteServiceTest.java
+++ src/test/java/com/revature/rideforce/maps/services/RouteServiceTest.java
@@ -1,13 +1,14 @@
 package com.revature.rideforce.maps.services;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -32,13 +33,20 @@ public class RouteServiceTest {
 	private Route route;
 	
 	@Test
-	
 	@Before
 	public void validate() {
 		assertNotNull(routeService);
 		assertTrue(routeService instanceof RouteService);
 	}
 	
+	@Test
+	public void goodRoute() {
+		Route correctRoute= routeService.getRoute("12160 Sunset Hills Rd, Reston, VA 20190", "905 Herndon Pkwy A, Herndon, VA 20170");
+//		assertTrue(correctRoute instanceof Route);
+//		assertThat(correctRoute, instanceOf(Route.class));
+		System.out.println(correctRoute.getDistance());
+//		assertTrue(correctRoute.getDistance()==3520);
+	}
 	
 	@Test
 	public void noStartParameter() {
@@ -51,4 +59,17 @@ public class RouteServiceTest {
 		Route badRoute = routeService.getRoute("11730 Plaza America Dr. Reston, VA","");
 		assertNull(badRoute);
 	}
+	
+	@Test
+	public void incompleteStartParameter() {
+		Route badRoute = routeService.getRoute("11730 Plaza America Dr.","12160 Sunset Hills Rd, Reston, VA 20190");
+		assertNull(badRoute);
+	}
+	
+	@Test 
+	public void incompleteEndParameter() {
+		Route badRoute = routeService.getRoute("12160 Sunset Hills Rd, Reston, VA 20190","12160 Sunset Hills");
+		assertNull(badRoute);
+	}
+	
 }
