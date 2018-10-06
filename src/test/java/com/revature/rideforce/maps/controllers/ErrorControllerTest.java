969cbedf1619a44886eca3b117d32610c47728aa
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-10-06 12:55:59 -0400

    testing error controller

--- src/test/java/com/revature/rideforce/maps/controllers/ErrorControllerTest.java
+++ src/test/java/com/revature/rideforce/maps/controllers/ErrorControllerTest.java
@@ -9,30 +9,44 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import com.revature.rideforce.maps.Application;
 import com.revature.rideforce.maps.beans.Route;
 import com.revature.rideforce.maps.controllers.RouteController;
 import com.revature.rideforce.maps.service.RouteService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ErrorController.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
 public class ErrorControllerTest {
 	
 	@Autowired
-	private MockMvc mvc;
+	private MockMvc mockMvc;
 	
 	@MockBean
 	private RouteService routeService;
 	
 	// THIS TEST SHOULD GO IN ROUTESERVICETEST
+//	@Test
+//	public void shouldReturn404() {
+//		Mockito.when(this.routeService.getRoute("", "")).thenThrow(MethodArgumentNotValidException.class);
+//	}
+	
 	@Test
-	public void shouldReturn404() {
-		Mockito.when(this.routeService.getRoute("", "")).thenThrow(MethodArgumentNotValidException.class);
+	public void testExceptionHandler() throws Exception {
+		this.mockMvc.perform(get("/error")
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .accept(MediaType.APPLICATION_JSON)
+	            )
+	            .andExpect(status().is5xxServerError());
 	}
 
 
