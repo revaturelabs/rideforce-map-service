b563643a3ab44a1d2113640ef364b682d646a509
Austen <austen.schips@gmail.com>
2018-10-08 10:15:57 -0400

    RouteService Tests

--- src/test/java/com/revature/rideforce/maps/services/RouteServiceTest.java
+++ src/test/java/com/revature/rideforce/maps/services/RouteServiceTest.java
@@ -3,7 +3,12 @@ package com.revature.rideforce.maps.services;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,30 +19,41 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.google.maps.GeoApiContext;
 import com.netflix.discovery.shared.Application;
+import com.revature.rideforce.maps.beans.Route;
 import com.revature.rideforce.maps.service.RouteService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class RouteServiceTest {
-	@Autowired
-	private GeoApiContext geoApiContext;
+	
 	@MockBean
 	private RouteService routeService;
 	
-	@Test
+	@Before
 	public void validate() {
 		assertNotNull(routeService);
 		assertThat(routeService, instanceOf(RouteService.class));
 	}
-//	@Test
-//	public Route testGet() throws Exception {
-//		routeService.getRoute("2925 Rensselaer Ct. Vienna, VA 22181", "2925 Rensselaer Ct. Vienna, VA 22181");
-//		return new Route(12714, 9600);
-//	}
-//	@Test
-//	public void testNegativeParams() throws Exception{
-//	routeService.getRoute("-80302", "80302");
-//}
+		
+		@Test
+		public void testGet() throws Exception {
+			final String start = "2925 Rensselaer Ct. Vienna, VA 22181";
+			final String end = "11730 Plaza America Dr. Reston, VA";
+			final Route route = new Route(12714, 9600);
+			final String routeJson = "{ distance: 12714, duration: 9600 }";
+
+			given(routeService.getRoute(start, end)).willReturn(route);
+			
+			Assert.assertEquals(routeService.getRoute(start, end), route);
+		}
+	
+	@Test
+	public void testNegativeParams() throws Exception{
+	given(routeService.getRoute("-80302", "80302")).willReturn(null);
+	
+	Assert.assertEquals(routeService.getRoute("-80302", "80302"), null);
+}
+	
 	
 
 	
