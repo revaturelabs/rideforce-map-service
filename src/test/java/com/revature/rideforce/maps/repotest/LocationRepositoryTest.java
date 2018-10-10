a57d70483878c6cea4c54dfe9b3c43ccb344e2cd
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-10-10 02:29:04 -0400

    additional repo tests; changed ddl back to update; coverage at 76.6%

--- src/test/java/com/revature/rideforce/maps/repotest/LocationRepositoryTest.java
+++ src/test/java/com/revature/rideforce/maps/repotest/LocationRepositoryTest.java
@@ -1,15 +1,13 @@
 package com.revature.rideforce.maps.repotest;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,15 +17,17 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 import com.revature.rideforce.maps.Application;
 import com.revature.rideforce.maps.beans.CachedLocation;
 import com.revature.rideforce.maps.repository.LocationRepository;
 
+/**
+ * These tests were created with the intention of being able to be run independently
+ * @author nozuk
+ *
+ */
 @SpringBootTest(classes = Application.class)
 @RunWith(SpringRunner.class)
 //@ContextConfiguration(classes=Application.class)
@@ -36,61 +36,107 @@ import com.revature.rideforce.maps.repository.LocationRepository;
 @Transactional
 public class LocationRepositoryTest {
 	
+	/**
+	 * The entity manager provided by Spring Data JPA for testing
+	 */
 	@Autowired
 	private TestEntityManager entityManager;
 	
+	/** 
+	 * The location repository
+	 */
 	@Autowired
 	private LocationRepository locationRepository;
 	
-	// assert that the beans get autowired 
+	/** 
+	 * Assert that the beans get autowired
+	 */
 	@Before
-	public void validate1() {
+	public void validate() {
 		assertNotNull(entityManager);
+	}	
+	
+	/**
+	 * At this point, db should not be empty
+	 */
+	@Test
+	public void databaseShouldNotBeEmpty() {
+		persistCachedLocations();
 		
-		CachedLocation cloc1 = new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533);
-		CachedLocation cloc2 = new CachedLocation("11733 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533);
-		CachedLocation cloc3 = new CachedLocation("11735 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533);
-		
-		entityManager.persist(cloc1);
-		entityManager.persist(cloc2);
-		entityManager.persist(cloc3);
+		List<CachedLocation> locations = locationRepository.findAll();
+		assertFalse(locations.size() == 0);
 	}
 	
+	/**
+	 * Three entries were added to db
+	 */
 	@Test
 	public void databaseShouldHaveThreeAddresses() {
+		persistCachedLocations();
+		
 		List<CachedLocation> locations = locationRepository.findAll();
-		assertFalse(locations.size() == 4);
 		assertTrue(locations.size() == 3);
 	}
 	
+	/**
+	 * No more than three entries should be in db
+	 */
+	@Test
+	public void databaseShouldNotHaveMoreThanThreeAddresses() {
+		persistCachedLocations();
+		
+		List<CachedLocation> locations = locationRepository.findAll();
+		assertFalse(locations.size() > 3);
+	}
 	
-//	@Test
-//	public void validate() {
-//		//assertThat(locationRepository).isNotNull();
-//		//CachedLocation loc1 = new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533);
-//		//CachedLocation loc2 = new CachedLocation("2150 Astoria Cir, Herndon, VA 20170",38.968193,-77.4164055);
-//		//locationRepository.save(loc2);
-//		//testEntityManager.persist(loc2);
-//		
-//		CachedLocation loc1 = locationRepository.save(new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533));
-//		System.out.println("the number of items in here:  ");
-//		Assertions.assertThat(locationRepository.findByAddress((loc1.getAddress()))).isEqualTo(loc1);
-//	}
-//	
-//	@Test
-//	public void testSaveAddress() {
-//		List<CachedLocation> locations = locationRepository.findAll();
-//		System.out.println("locations: " + locations);
-//        Assertions.assertThat(locationRepository).isNotNull();
-//		CachedLocation cLoc = locationRepository.save(new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533 ));
-//		Assertions.assertThat(locationRepository.findAll()).containsAnyOf(cLoc);
-//		
-//        this.entityManager.persist(new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533));
-//        CachedLocation loc = this.locationRepository.findByAddress("11730 Plaza America Dr #205, Reston, VA 20190");
-//        
-//        assertThat(loc.getAddress()).isEqualTo("11730 Plaza America Dr #205, Reston, VA 20190");
-//	}
+	/**
+	 * After removing all entries, database should be empty
+	 */
+	@Test
+	public void testRemoveCachedLocations() {
+		persistCachedLocations();
+		
+		// remove everything just added to db
+		entityManager.remove(locationRepository.findByAddress("11730 Plaza America Dr #205, Reston, VA 20190"));
+		entityManager.remove(locationRepository.findByAddress("2100 Astoria Cir, Herndon, VA 20170"));
+		entityManager.remove(locationRepository.findByAddress("503 Pride Ave, Herndon, VA 20170"));
+		
+		List<CachedLocation> locations = locationRepository.findAll();
+//		System.out.println();
+//		System.out.println("The current location Repository:  " + locations.toString());
+//		System.out.println();
+		assertTrue(locations.size() == 0);
+	}
 	
+	/**
+	 * After removing two entries, db should have one entry remaining
+	 */
+	@Test
+	public void testRemoveTwoCachedLocations() {
+		persistCachedLocations();
+		
+		entityManager.remove(locationRepository.findByAddress("11730 Plaza America Dr #205, Reston, VA 20190"));
+		entityManager.remove(locationRepository.findByAddress("503 Pride Ave, Herndon, VA 20170"));
+		
+		List<CachedLocation> locations = locationRepository.findAll();
+		assertTrue(locations.size() == 1);
+	}
+
+	/**
+	 * private helper method to add entries to db to avoid repeating code
+	 */
+	private void persistCachedLocations() {
+		CachedLocation cloc1 = new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533);
+		CachedLocation cloc2 = new CachedLocation("2100 Astoria Cir, Herndon, VA 20170",38.96787,-77.414742);
+		CachedLocation cloc3 = new CachedLocation("503 Pride Ave, Herndon, VA 20170",38.966271,-77.388985);
+		
+		// Keep in mind: the call of save on a detached instance creates a new persistent instance and assigns it a new identifier, 
+		// which results in a duplicate record in a database upon committing or flushing.
+		// Whereas, a second call to session.persist() would cause an exception
+		entityManager.persist(cloc1);
+		entityManager.persist(cloc2);
+		entityManager.persist(cloc3);
+	}	
 
 	/**
 	 * This tests if the data that was fetched from db is equal to the expected result, which is
@@ -101,9 +147,9 @@ public class LocationRepositoryTest {
 		CachedLocation cachedLocation = getCachedLocation();
 		CachedLocation savedInDb = entityManager.persist(cachedLocation);
 		// query by address because id for CachedLocation is address
-		CachedLocation getFromDb = locationRepository.findByAddress(savedInDb.getAddress());
+		CachedLocation fetchFromDb = locationRepository.findByAddress(savedInDb.getAddress());
 		
-		assertThat(getFromDb).isEqualTo(savedInDb);
+		assertThat(fetchFromDb).isEqualTo(savedInDb);
 	}
 	
 	/**
@@ -112,12 +158,37 @@ public class LocationRepositoryTest {
 	 */
 	private CachedLocation getCachedLocation() {
 		CachedLocation cLoc = new CachedLocation();
-		cLoc.setAddress("2100 Astoria Cir, Herndon, VA 20170");
-		cLoc.setLatitude(38.9677237);
-		cLoc.setLongitude(-77.4145711);
+		cLoc.setAddress("1070 Elden St, Herndon, VA 20170"); // Fresh World! :)
+		cLoc.setLatitude(38.9666958);
+		cLoc.setLongitude(-77.3975926);
 		return cLoc;
 	}
-//	
+	
+	/**
+	 * Test if an address is not in Db
+	 */
+	@Test
+	public void testAddressNotInDb() {
+		CachedLocation cachedLocation = getCachedLocation2();
+		CachedLocation savedInDb = entityManager.persist(cachedLocation);
+		// query by address because id for CachedLocation is address
+		CachedLocation fetchFromDb = locationRepository.findByAddress("1230 Elden St, Herndon, VA 20170");
+		
+		assertThat(fetchFromDb).isNotEqualTo(savedInDb);
+	}
+	
+	/**
+	 * private helper method to get cached location
+	 * @return the new CachedLocation object
+	 */
+	private CachedLocation getCachedLocation2() {
+		CachedLocation cLoc2 = new CachedLocation();
+		cLoc2.setAddress("1228 Elden St, Herndon, VA 20170"); // Giant! :)
+		cLoc2.setLatitude(38.9583948);
+		cLoc2.setLongitude(-77.3887017);
+		return cLoc2;
+	}
+	
 //	@Test
 //	public void canFindByThing2() {
 //		//List<CachedLocation> locations = locationRepository.findAll();
@@ -126,12 +197,38 @@ public class LocationRepositoryTest {
 //		CachedLocation cLoc = locationRepository.save(new CachedLocation("11730 Plaza America Dr #205, Reston, VA 20190",38.953414,-77.350533 ));
 //		Assertions.assertThat(locationRepository.findAll()).isNotNull();
 //	}
-//	
-//	@Test
-//    public void databaseShouldHaveThreeAddresses() {
-//        List<CachedLocation> cachedLocations = locationRepository.findAll();
-//        assertThat(cachedLocations.size() == 0);
-//        System.out.println(locationRepository.findAll());
-//    }
+	
+	/**
+	 * Should return null when there is invalid id (address)
+	 */
+	@Test
+	public void returnsNullWhenAddressIsEmptyString() {
+		persistCachedLocations();
+		
+		CachedLocation loc = locationRepository.findByAddress("");
+		assertThat(loc).isNull();
+	}
+	
+	/**
+	 * Should return null when there is invalid id (address)
+	 */
+	@Test
+	public void returnsNullWhenAddressIsOnlySpace() {
+		persistCachedLocations();
+		
+		CachedLocation loc = locationRepository.findByAddress(" ");
+		assertThat(loc).isNull();
+	}
+	
+	/**
+	 * Should return null when there is invalid id (address)
+	 */
+	@Test
+	public void returnsNullWhenAddressIsOnlyNumbers() {
+		persistCachedLocations();
+		
+		CachedLocation loc = locationRepository.findByAddress("2149");
+		assertThat(loc).isNull();
+	}
 	
 }
