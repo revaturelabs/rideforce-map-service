25951c0e5697eb0e48925cd2f0a3cae4a6b101cf
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-10-10 10:17:21 -0400

    removed @Transactional from above repository in LocationRepository

--- src/main/java/com/revature/rideforce/maps/repository/LocationRepository.java
+++ src/main/java/com/revature/rideforce/maps/repository/LocationRepository.java
@@ -1,7 +1,5 @@
 package com.revature.rideforce.maps.repository;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +12,6 @@ import com.revature.rideforce.maps.beans.CachedLocation;
  * @Repository
  */
 @Repository
-@Transactional
 public interface LocationRepository extends JpaRepository<CachedLocation, Integer> {
 	public CachedLocation findByAddress(String address);
 }
