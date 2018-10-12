046229091121ccd583b1d1c4f90171c99f77466b
Jorge_Sagrero <jsagreroperez@gmail.com>
2018-10-12 15:36:42 -0400

    update

--- src/test/java/com/revature/configuration/TestConfiguration.java
+++ src/test/java/com/revature/configuration/TestConfiguration.java
@@ -1,25 +1,39 @@
 package com.revature.configuration;
 
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.revature.rideforce.maps.repository.LocationRepository;
+import com.google.maps.GeoApiContext;
 
-@Configuration
-@EnableJpaRepositories("com.revature.rideforce.maps.repository")
-@EnableTransactionManagement
-@ComponentScan(basePackages = { "com.revature.rideforce"})
+
+//@Configuration
+//@EnableJpaRepositories("com.revature.rideforce.maps.repository")
+//@EnableTransactionManagement
+//@ComponentScan(basePackages = { "com.revature.rideforce"})
 // @ComponentScan(basePackages = "com.revature.rideforce", excludeFilters = @Filter(type = FilterType.REGEX, pattern = "com\\.revature\\.rideforce\\.maps\\.repository\\.config"))
+@RunWith(SpringJUnit4ClassRunner.class)
+@ComponentScan(basePackages=  {"com.revature.rideforce.maps.service","com.revature.rideforce.maps.configuration"})
+//@ContextConfiguration(classes= {GeoApiContext.class, ServiceTestConfiguration.class}, loader=AnnotationConfigContextLoader.class)
 public class TestConfiguration {
+	@Autowired
+	static private GeoApiContext service;
+	
+	@Test
+	public void notNull() {
+		assertThat(service).isNull();
+	
+//	@Autowired
+//	static private ServiceTestConfiguration service;
+//	
+//	@Test
+//	public void notNull() {
+//		assertThat(service).isNull();
+//	}
+	
 	
 }
