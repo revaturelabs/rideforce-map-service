6c3720ebfdfa8e088c2a8e666dfcc0e89cad8acd
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-10-12 21:18:44 -0400

    favorite feature doc + cleaning + needs constraint reworking

--- src/main/java/com/revature/rideforce/maps/controllers/FavoriteLocationController.java
+++ src/main/java/com/revature/rideforce/maps/controllers/FavoriteLocationController.java
@@ -2,16 +2,13 @@ package com.revature.rideforce.maps.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,17 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.maps.model.LatLng;
 import com.revature.rideforce.maps.beans.FavoriteLocation;
-import com.revature.rideforce.maps.beans.ResponseError;
 import com.revature.rideforce.maps.service.FavoriteLocationService;
 
 /**
  * The controller for obtaining the favorited locations
  * @author Revature Java batch
  * @RestController
- * @RequestMapping(value = "/favoritelocations/users")
+ * @RequestMapping(value = "/favoritelocations")
  */
 @RestController
-@RequestMapping(value = "/favoritelocations/users")
+@RequestMapping(value = "/favoritelocations")
 public class FavoriteLocationController {
 	
 	/**
@@ -43,7 +39,7 @@ public class FavoriteLocationController {
 	 * @Autowired
 	 */
 	@Autowired
-	private FavoriteLocationService favoriteLocationService;
+	private FavoriteLocationService fls;
 	
 	/**
 	 * list of favorite locations
@@ -53,19 +49,18 @@ public class FavoriteLocationController {
 	/** 
 	 * This method finds favorite locations by user id then limits that list to 5 locations
 	 * @param userId
-	 * @return List<FavoriteLocation>
+	 * @return List<FavoriteLocation> (the list of favorite locations)
 	 */
-	@GetMapping(value="/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<FavoriteLocation> getFavoriteLocationsByUserId(@PathVariable("userId") int userId, @RequestParam String address){
-		log.info("finding locations by user");
-		if(favoriteLocationService.findFavoriteLocationByUserId(userId).size() > 5) {
-			log.debug("User with id: " + userId + "tried to favorite more than 5 locations");
-			return null;
-		}
-		else {
-			return favoriteLocations.stream().filter(location -> location.getUserId() == userId).collect(Collectors.toList());
-
-		}
-
-	}
+	@RequestMapping(value="/users/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<?> getLocationsByUserId(@RequestParam String address, @PathVariable("id") int userId){
+        log.info("finding locations by user");
+        if(fls.findFavoriteLocationByUserId(userId).size() > 5) {
+            log.debug("User with id: " + userId + "tried to favorite more than 5 locations");
+            return null;
+        }
+        else {
+            return new ResponseEntity<LatLng>(fls.getOne(address, userId), HttpStatus.OK);
+        }
+    
+    }
 }
