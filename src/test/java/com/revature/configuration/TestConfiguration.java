d61ddcc947fb47ef777c364fbac7e50ef6db4922
Jonathan Novoa <jon142536@gmail.com>
2018-10-15 16:03:58 -0400

    more extra code

--- src/test/java/com/revature/configuration/TestConfiguration.java
+++ src/test/java/com/revature/configuration/TestConfiguration.java
@@ -6,18 +6,12 @@ import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.google.maps.GeoApiContext;
-import com.revature.rideforce.maps.repository.LocationRepository;
 
 @Configuration
 @RunWith(SpringJUnit4ClassRunner.class)
