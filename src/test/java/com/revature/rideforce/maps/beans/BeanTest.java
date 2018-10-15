d137dc674c758fdba6cae604551505fd133f7fe4
Austen <austen.schips@gmail.com>
2018-10-15 16:23:32 -0400

    new bean tests

--- src/test/java/com/revature/rideforce/maps/beans/BeanTest.java
+++ src/test/java/com/revature/rideforce/maps/beans/BeanTest.java
@@ -2,6 +2,7 @@ package com.revature.rideforce.maps.beans;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -15,6 +16,9 @@ import javax.validation.ValidatorFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.google.maps.model.LatLng;
+
 import org.junit.Assert;
 public class BeanTest{
 	private static ValidatorFactory validatorFactory;
@@ -148,6 +152,13 @@ public class BeanTest{
         assertThat(location1.equals(location2));
         assertThat(location.equals(location2));
     }
+    
+    @Test
+    public void locationGetterTest() {
+        CachedLocation location = new CachedLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35);
+    	
+        assertEquals(location.getLocation(), new LatLng(38.95, -77.35));
+    }
    
     
     @Test
@@ -174,6 +185,11 @@ public class BeanTest{
     @Test
     public void locationConstructorTest() {
     	new CachedLocation();
+    }
+    
+    @Test
+    public void locationConstructorTest2() {
+    	new CachedLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35));
     }
     
     @Test
@@ -210,5 +226,157 @@ public class BeanTest{
         location.setLongitude(-77.35);
         assertThat(location.getLongitude()==-77.35);
     }
+    
+   //FavoriteLocation Bean Tests 
+   
+    @Test
+    public void validateFavoriteLocationTest() {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "work", 1);
+        Set<ConstraintViolation<FavoriteLocation>> violations
+            = validator.validate(location);
+
+        assertTrue(violations.isEmpty());
+    }
+    
+    @Test
+    public void violateFavoriteConstraintTest() {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "work", 1);
+        Set<ConstraintViolation<FavoriteLocation>> violations
+            = validator.validate(location);
+     
+        assertEquals(1, violations.size());
+     
+        ConstraintViolation<FavoriteLocation> violation
+            = violations.iterator().next();
+        assertEquals("size must be between 0 and 85",
+                     violation.getMessage());
+        assertEquals("address", violation.getPropertyPath().toString());
+        assertEquals("2925 Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Rensselaer Ct. Vienna, VA 22181", violation.getInvalidValue());
+    }
+    
+    @Test
+    public void favoriteLocationConstructorTest() {
+    	new FavoriteLocation();
+    }
+    
+    @Test
+    public void favoriteLocationSetterAndGetterTest() {
+    	FavoriteLocation location = new FavoriteLocation();
+        location.setAddress("2925 Rensselaer Ct. Vienna, VA 22181");
+        assertEquals(location.getAddress(), "2925 Rensselaer Ct. Vienna, VA 22181");
+    }
+    
+    @Test
+    public void favoriteLocationSetterAndGetterTest2() {
+    	FavoriteLocation location = new FavoriteLocation();
+        location.setLatitude(38.95);
+        assertThat(location.getLatitude()==38.95);
+    }
+    
+    @Test
+    public void favoriteLocationSetterAndGetterTest3() {
+    	FavoriteLocation location = new FavoriteLocation();
+        location.setLongitude(-77.35);
+        assertThat(location.getLongitude()==-77.35);
+    }
+    
+    @Test
+    public void favoriteLocationSetterAndGetterTest4() {
+        FavoriteLocation favoriteLocation = new FavoriteLocation();
+        favoriteLocation.setName("home");
+        assertEquals(favoriteLocation.getName(), "home");
+    }
+    
+    @Test
+    public void favoriteLocationSetterAndGetterTest5() {
+        FavoriteLocation favoriteLocation = new FavoriteLocation();
+        favoriteLocation.setUserId(1);
+        assertEquals(favoriteLocation.getUserId(), 1);
+    }
+    
+    @Test
+    public void favoriteLocationSetterAndGetterTest6() {
+    	FavoriteLocation favoriteLocation = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+        assertEquals(favoriteLocation.getFavoriteLocation(), new LatLng(38.95, -77.35));
+    }
+    
+    @Test
+    public void favoriteLocationToStringTest()
+    {
+    		
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+        String expected = "FavoriteLocation [address=" + "2925 Rensselaer Ct. Vienna, VA 22181" + ", latitude=" + 38.95 + ", longitude=" + -77.35
+				+ ", name=" + "home" + ", userId=" + 1 + "]";
+        Assert.assertEquals(expected, location.toString());
+    }
+    
+    @Test
+    public void favoriteLocationEqualsReflexive(){	
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+        assertEquals(location, location);
+    }
+    
+    @Test
+    public void favoriteLocationEqualsSymmetric() {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+    	FavoriteLocation location1 = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+        assertThat(location.equals(location1));
+        assertThat(location1.equals(location));
+    }
+    
+    @Test
+    public void favoriteLocationEqualsTransitive() {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+    	FavoriteLocation location1 = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+    	FavoriteLocation location2 = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", 38.95, -77.35, "home", 1);
+        assertThat(location.equals(location1));
+        assertThat(location1.equals(location2));
+        assertThat(location.equals(location2));
+    }
+    
+    @Test
+    public void favoriteLocationEqualsReflexive2(){	
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+        assertEquals(location, location);
+    }
+    
+    @Test
+    public void favoriteLocationEqualsSymmetric2() {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+    	FavoriteLocation location1 = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+        assertThat(location.equals(location1));
+        assertThat(location1.equals(location));
+    }
+    
+    @Test
+    public void favoriteLocationEqualsTransitive2() {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+    	FavoriteLocation location1 = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+    	FavoriteLocation location2 = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+        assertThat(location.equals(location1));
+        assertThat(location1.equals(location2));
+        assertThat(location.equals(location2));
+    }
+    
+    @Test
+    public void hashCodeFLConsistencyTest()
+    {
+    	FavoriteLocation location = new FavoriteLocation("2925 Rensselaer Ct. Vienna, VA 22181", new LatLng(38.95, -77.35), 1);
+       
+        assertEquals(location.hashCode(), location.hashCode());
+        
+    }
+    
+    @Test
+    public void hashCodeFLTest()
+    {
+    	String address = "2925 Rensselaer Ct. Vienna, VA 22181";
+    	FavoriteLocation location = new FavoriteLocation(address, new LatLng(38.95, -77.35), 1);
+    	FavoriteLocation location1 = new FavoriteLocation(address, new LatLng(38.95, -77.35), 1);
+        assertEquals(location.hashCode(), location1.hashCode());
+        
+    }
+    
+    
     
 }
