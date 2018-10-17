8a74859ad83d1cc066de3ef3c1b48e2bff970817
Jonathan Novoa <jon142536@gmail.com>
2018-10-17 17:35:30 -0400

    Cross origin on all controllers

--- src/main/java/com/revature/rideforce/maps/controllers/MapsTestController.java
+++ src/main/java/com/revature/rideforce/maps/controllers/MapsTestController.java
@@ -1,5 +1,6 @@
 package com.revature.rideforce.maps.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @RestController
  */
 @RestController
+@CrossOrigin
 public class MapsTestController {
 
 	/**
