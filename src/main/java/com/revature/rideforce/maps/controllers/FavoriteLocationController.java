ac00717c4a1efdf82077581979e9591d600644ad
Jonathan Novoa <jon142536@gmail.com>
2018-10-17 17:15:01 -0400

    Cross origin annotation

--- src/main/java/com/revature/rideforce/maps/controllers/FavoriteLocationController.java
+++ src/main/java/com/revature/rideforce/maps/controllers/FavoriteLocationController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.revature.rideforce.maps.service.FavoriteLocationService;
  * @RequestMapping(value = "/favoritelocations")
  */
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/favoritelocations")
 public class FavoriteLocationController {
 	
