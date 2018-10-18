08d37ed5dfd75336526d80da42321be69c328d7c
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-10-17 21:46:32 -0400

    fixing tests and more documentation and some code cleaning

--- src/test/java/com/revature/rideforce/maps/controllers/FavoriteLocationControllerTest.java
+++ src/test/java/com/revature/rideforce/maps/controllers/FavoriteLocationControllerTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.google.maps.model.LatLng;
-
 import com.revature.rideforce.maps.service.FavoriteLocationService;
 
 @RunWith(SpringRunner.class)
