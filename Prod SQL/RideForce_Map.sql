6c358f74f3176f40936f47dd273bdb0462794442
Warren Chan <warrenchan31@gmail.com>
2019-01-28 16:40:54 -0500

    map schema and erd

---
+++ Prod SQL/RideForce_Map.sql
@@ -0,0 +1,21 @@
+/* map-service */ 
+
+drop table address cascade constraints;
+drop table favorite_addresses cascade constraints;
+
+create table address (
+	address varchar2(255 char) not null primary key,
+	latitude float(126),
+	longitude float(126)
+);
+
+create table favorite_addresses (
+	location_id number(10) not null primary key,
+	address varchar2(85 char),
+	latitude float(126),
+	longitude float(126),
+	favorited_location_name varchar2(255 char),
+	user_id number(10)
+);
+
+commit;
