628b90cdf5fa420b74e31979419a56f03dda8e6e
Warren Chan <warrenchan31@gmail.com>
2019-01-29 10:36:15 -0500

    removed the drop table of favorite

--- Prod SQL/RideForce_Map.sql
+++ Prod SQL/RideForce_Map.sql
@@ -1,14 +1,13 @@
 /* map-service */ 
 
 drop table address cascade constraints;
-drop table favorite_addresses cascade constraints;
 
 create table address (
 	cached_id number not null primary key,
 	address varchar2(85 char) not null,
 	city varchar2(85 char),
 	state_code varchar2(2 char),
-	zip_code varchar2,
+	zip_code varchar2(85 char),
 	latitude float(126),
 	longitude float(126)
 );
