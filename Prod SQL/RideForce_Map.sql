e3a6ec844e542818259d9c52780d4f19a0775f83
Warren Chan <warrenchan31@gmail.com>
2019-01-29 10:28:39 -0500

    edited address to be more like the beans and removed favorite address as it wasnt in the bean

--- Prod SQL/RideForce_Map.sql
+++ Prod SQL/RideForce_Map.sql
@@ -4,18 +4,13 @@ drop table address cascade constraints;
 drop table favorite_addresses cascade constraints;
 
 create table address (
-	address varchar2(255 char) not null primary key,
+	cached_id number not null primary key,
+	address varchar2(85 char) not null,
+	city varchar2(85 char),
+	state_code varchar2(2 char),
+	zip_code varchar2,
 	latitude float(126),
 	longitude float(126)
-);
-
-create table favorite_addresses (
-	location_id number(10) not null primary key,
-	address varchar2(85 char),
-	latitude float(126),
-	longitude float(126),
-	favorited_location_name varchar2(255 char),
-	user_id number(10)
 );
 
 drop sequence hibernate_sequence;
