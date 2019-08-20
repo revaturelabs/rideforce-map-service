95339b36887dbf1a2c5ec116f17ed7d105c48d2c
billyclara <billy.clara@baruchmail.cuny.edu>
2019-08-20 15:56:11 -0400

    code restructuring

--- src/main/java/com/revature/rideforce/maps/Application.java
+++ src/main/java/com/revature/rideforce/maps/Application.java
@@ -1,7 +1,9 @@
 package com.revature.rideforce.maps;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * The starting point of spring application
